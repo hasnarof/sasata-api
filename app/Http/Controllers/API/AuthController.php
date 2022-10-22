c2c212bf5faceb2003d4ef1d3281640eb2297b76
Fairuz Hasna Rofifah <hasnarof@gmail.com>
2022-10-22 09:33:21 +0700

    feat: authentication sanctum

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Auth;
+use Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'error' => true,
+                    'message' => $validator->errors()
+                ]
+            );
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'error' => false,
+                'data' => $user, 'access_token' => $token, 'token_type' => 'Bearer',
+            ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()
+                ->json([
+                    'error' => true,
+                    'message' => 'Unauthorized'
+                ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'error' => false,
+                'message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer',
+            ]);
+    }
+
+    // method for user logout and delete token
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'error' => false,
+            'message' => 'You have successfully logged out and the token was successfully deleted'
+        ];
+    }
+}
